f02fbe98fed550873fd44b31a33f91a50027d49b
vaclavek-kamil <118657567+vaclavek-kamil@users.noreply.github.com>
2023-01-27 15:48:41 +0100

    Attack - hitbox
    The attack function creates a rectangle which will be used as a hitbox to detect attack hits. No animation yet - just an outlined rectangle.

--- players.py
+++ players.py
@@ -26,12 +26,20 @@ class Player (pygame.sprite.Sprite):
         self.rect.width -= TILE_SIZE*2
         self.rect.height -= TILE_SIZE*2
         self.rect.topleft = pos
+        self.attack_hitbox = None
 
         #AMOUNT OF FRAMES, BEFORE THE ATTACK IS EXECUTED AFTER CHARGING
-        self.CHARGE_DURATION = 15
+        self.CHARGE_DURATION = 20
 
         #THE AMOUNT OF FRAME A DODGE LAST, MAKING THE PLAYER IMUNE TO DAMAGE (IGNORES HITS)
         self.DODGE_DURATION = 25
+
+        #THE AMOUNT OF PIXELS AN ATTACK REACHES
+        self.ATTACK_RANGE = 40
+
+        #ATTACK PROGRESS -  (0 - CHARGE_DURATION) = THE ATTACK IS CHARGING
+        ##################  ((CHAGRE_DURATION + 1 - (2*CHARGE_DURATION)) = THE ATTACK HITBOX IS ACTIVE
+        self.attack_progress = 0
 
         #HEALTH POINTS
         self.hp = 100
@@ -62,10 +70,84 @@ class Player (pygame.sprite.Sprite):
         pygame.Surface.blit( level.display_surface, chosen_image, rotated_rect)
         
         #IMAGE FRAME DEBUG
-        pygame.draw.rect(level.display_surface, (255,255,0), rotated_rect, 3)
+        #pygame.draw.rect(level.display_surface, (255,255,0), rotated_rect, 3)
         
         #HITBOX DEBUG
         pygame.draw.rect(level.display_surface, ((self.NUMBER-1)*255,(self.NUMBER-2)*-255,0), self.rect, 3)
+
+        #Attack hitbox debug
+        if self.attack_hitbox != None:
+            pygame.draw.rect(level.display_surface, (255,255,0), self.attack_hitbox, 3)
+
+#####################################################################################################################################################################################################
+#   ATTACK FUNCTION FOR THE PLAYER USED IN THE MAIN UPDATE FUNCTION
+#####################################################################################################################################################################################################
+    def attack(self, level):
+        #START THE ATTACK
+        if ((level.pressed_keys[pygame.K_g] and self.NUMBER == 1) or (level.pressed_keys[pygame.K_m] and self.NUMBER == 2)) and self.attack_progress == 0:
+            self.attack_progress += 1
+            return True
+
+        #ATTACK CHARING
+        elif self.attack_progress > 0 and self.attack_progress <= self.CHARGE_DURATION:
+            self.attack_progress += 1
+            return True
+
+        #ATTACK HIT DETECTION
+        elif self.attack_progress > self.CHARGE_DURATION:
+            self.attack_progress += 1
+            
+            print(str(self.angle) + '   ' + str(self.attack_progress))
+
+            if self.angle == 0:
+                offset_x = 0
+                offset_y = self.ATTACK_RANGE
+
+            elif self.angle == 45:
+                offset_x = math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+                offset_y = math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+
+            elif self.angle == 90:
+                offset_x = self.ATTACK_RANGE
+                offset_y = 0
+
+            elif self.angle == 135:
+                offset_x = math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+                offset_y = -math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+
+            elif self.angle == 180:
+                offset_x = 0
+                offset_y = -self.ATTACK_RANGE
+
+            elif self.angle == 225:
+                offset_x = -math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+                offset_y = -math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+
+            elif self.angle == 270:
+                offset_x = -self.ATTACK_RANGE
+                offset_y = 0
+
+            elif self.angle == 315:
+                offset_x = -math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+                offset_y = math.sqrt(self.ATTACK_RANGE*self.ATTACK_RANGE/2)
+            
+            offset_x = round(offset_x)
+            offset_y = round(offset_y)
+            
+            print(str(offset_x) + '   ' + str(offset_y))
+
+            self.attack_hitbox = pygame.Rect(self.rect.x + offset_x, self.rect.y + offset_y, self.rect.width, self.rect.height)
+            
+            #end the attack
+            if self.attack_progress > 2*self.CHARGE_DURATION:
+                self.attack_progress = 0
+                self.attack_hitbox = None
+
+
+
+        
+
+        
 
 #####################################################################################################################################################################################################
 #   MOVEMENT FUNCTION FOR THE PLAYER USED IN THE MAIN UPDATE FUNCTION
@@ -123,3 +205,4 @@ class Player (pygame.sprite.Sprite):
     def update(self, level):
 
         self.movement(level)
+        self.attack(level)
